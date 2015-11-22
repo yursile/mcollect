cc5c638fd02ba9efc2afbec15864827d547c80e4
yursile <289181450@qq.com>
2015-11-22 19:07:45 +0800

    use zepto

---
+++ src/js/h5_data.js
@@ -0,0 +1,111 @@
+ var main_h5 = [{
+ 	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	title:'1一秒钟到底有多长?HOW',
+	content:'1一秒钟到底有多长',
+	url:"m.sohu.com/c/17568/"
+ }];
+
+ var h5_data = [
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'1一秒钟到底有多长?HOW',
+	        	content:'1一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'2一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'3一秒钟到底有多长?SECOND?',
+	        	content:'3一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'3一秒钟到底有OW LONG IS ONE SECOND?',
+	        	content:'4一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'5一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'5一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'6一秒钟到底S ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'7一秒钟到底有多 LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'8一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'9一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'10一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'11一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'12一秒钟到底有多长?HOW',
+	        	content:'1一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'13一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'14一秒钟到底有多长?SECOND?',
+	        	content:'3一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'15一秒钟到底有OW LONG IS ONE SECOND?',
+	        	content:'4一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'16一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	content:'5一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/"
+	        },
+	        {
+	        	img:'http://news.sohu.com/upload/yursiletestPage1020/img/small.png',
+	        	title:'17一秒钟到底S ONE SECOND?',
+	        	content:'一秒钟到底有多长',
+	        	url:"m.sohu.com/c/17568/",
+	        }
+	        ];
